eb4eb630922c4831c7ad8a523059572371577948
NathanMaupin <31774996+NathanMaupin@users.noreply.github.com>
2021-10-16 23:18:59 -0500

    Finished Problem 1

--- Problem 1.py
+++ Problem 1.py
@@ -1,7 +1,7 @@
 """
-1. Least-squares (20 pts) Code.Create  a  set  ofN=  100  points,xk,  uniformly  distributed in the [−1,+1] range.
+1. Least-squares (20 pts) Code.Create  a  set  ofN=  100  points,xk,  uniformly  distributed in the [âˆ’1,+1] range.
 Compute the function f(x) = sin(3x) at the xk data points.  Corrupt the f(xk) values by adding a Gaussian noise with
-standard deviation σ= 0.08.  Plo f(xk) and fc(xk).  Perform the least-squares of fc(xk) using the fitting functions,
+standard deviation Ïƒ= 0.08.  Plo f(xk) and fc(xk).  Perform the least-squares of fc(xk) using the fitting functions,
 """
 import math
 import numpy as np
@@ -16,10 +16,7 @@ deviation = 0.08
 # Creates array of 100 points for N
 while N_iteration <= 1:
     N_iteration = round(N_iteration, 2)
-    #noise = np.random.normal(0, .1, None)
-    #noise_iteration = N_iteration + noise
     N.append(N_iteration)
-    #N_noise.append(noise_iteration)
     N_iteration += 0.02
 
 
@@ -31,11 +28,11 @@ def f(x):
 # Plots f(xk) and fc(xk)
 count = 0
 fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-noise = np.random.normal(0, .1, None)
+graph1 = fig1.add_subplot(111)
+noise = np.random.normal(0, .08, None)
 while count < 100:
-    ax1.scatter(N[count], f(N[count]), color="blue", s=10, label="f(xk)" if count == 0 else "")
-    ax1.scatter(N[count], f(N[count]) + noise, color="green", s=10, label="fc(xk)" if count == 0 else "")
+    graph1.scatter(N[count], f(N[count]), color="blue", s=10, label="f(xk)" if count == 0 else "")
+    graph1.scatter(N[count], f(N[count]) + noise, color="orange", s=10, label="fc(xk)" if count == 0 else "")
     count += 1
 plt.title("f(xk) vs. fc(xk)")
 plt.xlabel("x-axis")
@@ -43,15 +40,59 @@ plt.ylabel("y-axis")
 plt.legend()
 plt.show()
 
-a = []
-b = []
+
+#Least Square with f1(x)
+fig2 = plt.figure()
+graph2 = fig2.add_subplot(111)
+a1 = []
+b1 = []
+for n1 in N:
+    a1.append([1, n1, 2*n1**2 - 1, 4*n1**3 - 3*n1])
+    b1.append(f(n1) + noise)
+
+c1, residuals, rank, s = np.linalg.lstsq(a1, b1, rcond=None)
+
+
+def f1(x):
+    return c1[0] + c1[1]*x +c1[2]*(2*x**2 - 1) + c1[3]*(4*x**3 - 3*x)
+
+#Least Square with f2(x)
+a2 = []
+b2 = []
+for n2 in N:
+    a2.append([math.sin(n2**2), 1 - math.cos(n2), math.cos(n2)*math.sin(n2), (2-n2)/(2+n2)])
+    b2.append(f(n2) + noise)
+
+c2, residuals, rank, s = np.linalg.lstsq(a2, b2, rcond=None)
+
+def f2(x):
+    return c2[0]*math.sin(x**2) + c2[1]*(1 - math.cos(x)) +c2[2]*(math.cos(x)*math.sin(x)) + c2[3]*((2-x)/(2+x))
+
+f1y = []
+f2y = []
 for n in N:
-    a.append([1, n, 2*n**2 - 1, 4*n**3, -3*n])
-    b.append(f(n) + noise)
-
-x = np.linalg.lstsq(a, b, rcond=None)
-print("###########@###")
-print(x)
+    f1y.append(f1(n))
+    f2y.append(f2(n))
     
+plt.title("f1(x) vs. f2(x)")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+graph2.scatter(N, f1y, s=10, color="black", label="f1(x)")
+graph2.scatter(N, f2y, s=10, color="red", label="f2(x)")
+plt.legend()
+plt.show()
 
+#Calculating Norms
 
+norm11 = np.linalg.norm(np.array(b1) - np.array(f1y), ord=1)
+norm21 = np.linalg.norm(np.array(b2) - np.array(f2y), ord=1)
+norm12 = np.linalg.norm(np.array(b1) - np.array(f1y), ord=2)
+norm22 = np.linalg.norm(np.array(b2) - np.array(f2y), ord=2)
+norm1inf = np.linalg.norm(np.array(b1) - np.array(f1y), ord=np.inf)
+norm2inf = np.linalg.norm(np.array(b2) - np.array(f2y), ord=np.inf)
+print("f1(x) 1st Norm:", norm11)
+print("f2(x) 1st Norm:", norm21)
+print("f1(x) 2nd Norm:", norm12)
+print("f2(x) 2nd Norm:", norm22)
+print("f1(x) Infinite Norm:", norm1inf)
+print("f2(x) Infinite Norm:", norm2inf)
