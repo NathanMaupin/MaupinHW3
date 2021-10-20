c84a3a83f55cf8c0ddc6af7af570a4a11a98985a
NathanMaupin <31774996+NathanMaupin@users.noreply.github.com>
2021-10-19 23:52:16 -0500

    Working on 3

--- problem3.py
+++ problem3.py
@@ -12,9 +12,12 @@ x0 = 0.1
 y0 = 0.1
 z0 = -0.1
 h = 1e-8
-vk = (x0, y0, z0)
+vk = np.array([x0, y0, z0])
+vk1 = np.array([9999999, 9999999, 999999])
+v_diff = np.subtract(vk1, vk)
 A = []
 b = []
+
 
 def f1(x, y, z):
     return(3*z - math.sin(x*y) - 1/8)
@@ -25,7 +28,7 @@ def f2(x, y, z):
 
 
 def f3(x, y, z):
-    return(math.exp(y) + 20*x*z**2 + math.pi/4)
+    return(math.exp(-y) + 20*x*z**2 + math.pi/4)
 
 
 def fprime(f, x, y, z):
@@ -34,14 +37,20 @@ def fprime(f, x, y, z):
     dz = (f(x, y, z+h) - f(z, y, z-h))/(2*h)
     return dx, dy, dz
 
-#fprime(f1, 1, 1, 1)
-
-
-while np.linalg.norm(vk) < h:
-    A.extend(fprime(f1, x0, y0, z0), fprime(f2, x0, y0, z0), fprime(f3, x0, y0, z0))
-    b.extend(f1(x0, y0, z0), f2(x0, y0, z0), f3(x0, y0, z0))
-    print(A)
-    print(b)
-    print(test
-    xbar = np.linalg.solve(A, b)
-    print(xbar)
+count = 0
+while np.linalg.norm(v_diff, ord=2) > h:
+    print(x0)
+    print(count)
+    A.append(fprime(f1, x0, y0, z0))
+    A.append(fprime(f2, x0, y0, z0))
+    A.append(fprime(f3, x0, y0, z0))    
+    b.append(f1(x0, y0, z0))    
+    b.append(f2(x0, y0, z0))
+    b.append(f3(x0, y0, z0))
+    vk = np.array([x0, y0, z0])
+    x0, y0, z0 = np.linalg.solve(A, b)
+    vk1 = np.array([x0, y0, z0])
+    v_diff = np.subtract(vk1, vk)
+    A = []
+    b = []
+    count += 1
